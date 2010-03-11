0caef7677ee82af0db79c9368958481efc3d5a04
naves.thiago <naves.thiago@1de104e0-902e-11de-b034-ff552ed6835c>
2010-03-11 20:05:59 +0000

    PC_Keyboard: Started working on receive function.

--- trunk/pc_keyboard/keyboard.lua
+++ trunk/pc_keyboard/keyboard.lua
@@ -11,7 +11,7 @@ local P_DATA    = pio.pin.P0_2 -- Data
 local P_CLK_PD  = pio.pin.P0_3 -- Clock pull down
 local P_DATA_PD = pio.pin.P0_4 -- Data pull down
 
-function init( u_id )
+function init()
   pio.pin.setdir( pio.OUTPUT, P_CLK_PD, P_DATA_PD )
 end
 
@@ -27,6 +27,43 @@ function send( data )
 
 end
 
-function receive( timer )
+function receive( timer, timeout )
+  pio.pin.setdir( pio.INPUT, P_CLK, P_DATA )
+  enable_communication()
+
+  tmr.setclock( timer, 1000 )
+  local tc = tmr.start( timer )
+
+  local read = false
+
+  -- Wait for the keyboard to begin to send, or timeout
+  while not read do
+    if pio.pin.getval( P_CLK ) == 0 and pio.pin.getval( P_DATA ) then
+      read = true
+    end
+
+    tmr.read( timer )
+    if tmr.read( timer ) == timeout + tc then
+      return nil
+    end
+  end
+
+  tmr.setclock( timer, 10000 )
+  tc = tmr.start( timer )
+
+  -- Wait for the clock to go Up or timeout
+  while pio.pin.getval( P_CLK ) == 0 and tmr.read( timer ) - tc < 1 do
+  end
+
+  local data = 0
+  -- Data is valid when Clock goes Down
+  while tmr.read( timer ) - tc < 1 do
+    if pio.pin.getval( P_CLK ) == 0 then
+      tc = tmr.start( timer )
+      data = bit.lshift( data, 1 )
+      data = bit.bor( data, pio.pin.getval( P_DATA ) )
+    end
+    -- Check start bit, end bit, check bit
+  end
 
 end 
